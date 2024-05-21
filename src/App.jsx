f87b90fe48db9168e3bb9586b90868b28b800f19
Aman Nagar <amannagar4870@gmail.com>
2024-05-21 16:51:47 +0530

    setup use auth

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter basename="/blog/">
+    <BrowserRouter basename="/blog">
       <Header user={user} />
       <Routes>
         <Route path="/" element={<Home />} />
